2bcd2f70c1588428d4101c092a3914996b1e09cb
Guilleloper <pgaugusto24@gmail.com>
2024-11-20 18:01:17 -0500

    2024-11-20

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,4 +1,5 @@
 ﻿using BuildingBlocks.CQRS;
+using FluentValidation;
 using Marten;
 
 namespace Catalog.API.Products.UpdateProduct
@@ -6,6 +7,21 @@ namespace Catalog.API.Products.UpdateProduct
     public record UpdateProductCommad(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<UpdateProductResult>;
     public record UpdateProductResult(bool IsSuccess);
+
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommad>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(command => command.Id).NotEmpty().WithMessage("Product ID is required");
+
+            RuleFor(command => command.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+
+            RuleFor(command => command.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
 
     internal class UpdateProductCommandHandler
         (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
@@ -19,7 +35,7 @@ namespace Catalog.API.Products.UpdateProduct
 
             if (product is null)
             {
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(command.Id);
             }
 
             product.Name = command.Name;
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,21 +1,53 @@
+using BuildingBlocks.Behaviors;
 using BuildingBlocks.Extensions;
 using Catalog.API.Extensions;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
-builder.Services.AddCarterWithAssemblies(typeof(Program).Assembly);
+
 builder.Services.AddMartenExtension(builder.Configuration);
 builder.Services.AddValidatorExtension();
+var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+builder.Services.AddCarterWithAssemblies(assembly);
 
 var app = builder.Build();
 
 //Configure the HTTP request pipeline
 app.MapCarter();
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null)
+        {
+            return;
+        }
 
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetService<ILogger<Program>>();
+        logger!.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+
+    });
+});/*manejo excepciones de forma global*/
 
 app.Run();
